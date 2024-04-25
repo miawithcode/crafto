cd4b94f2e7487b0dbf92b98720b16fbf2a511bdd
miawithcode <cuttontail@gmail.com>
2024-04-25 20:54:35 +0200

    Init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      white: '#FFFFFF',
+      rose: '#FC5C7D',
+      purple: '#6A82FB',
+      divider: '#0C0D0E',
+      bgBody: '#fafafa',
+      bgGray: '#f3f4f6',
+      bgBlack: '#1A1A1A',
+      bgLightGray: '#e5e7eb',
+      bgBlackLight: '#262626',
+      textBlack: '#0C0D0E',
+      textMuted: '#6D6D6E',
+      transparent: 'transparent',
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
       },
     },
+    extend: {},
   },
   plugins: [],
 };
