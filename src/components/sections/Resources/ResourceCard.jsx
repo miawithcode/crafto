664a29284de808d6add12bed59ddd4b5bdb82e99
miawithcode <cuttontail@gmail.com>
2024-04-26 11:41:15 +0300

    style: remove resource card overlay

--- src/components/sections/Resources/ResourceCard.jsx
+++ src/components/sections/Resources/ResourceCard.jsx
@@ -1,4 +1,3 @@
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
 const ResourceCard = ({ resource }) => {
@@ -8,14 +7,14 @@ const ResourceCard = ({ resource }) => {
     <a
       target="_blank"
       href={url}
-      className="group group relative rounded-lg bg-bgGray px-5 py-4 shadow-sm transition hover:shadow-md"
+      className="relative rounded-lg bg-bgGray px-5 py-4 shadow-sm transition hover:shadow-md"
     >
       <div className="relative h-[25vh] w-full lg:h-[20vh]">
         <Image
           className="rounded-xl object-cover object-center"
           src={`https:${image.fields.file.url}`}
           alt={title}
-          fill
+          fill  
         />
       </div>
 
@@ -24,11 +23,6 @@ const ResourceCard = ({ resource }) => {
         <p className="pt-2 text-sm tracking-tight text-textMuted">
           {description}
         </p>
-      </div>
-
-      {/* Hover Overlay */}
-      <div className="absolute bottom-0 left-0 hidden h-1/6 w-full rounded-b-lg bg-bgBlack text-white group-hover:flex group-hover:justify-center group-hover:items-center">
-        Visit site <ArrowTopRightOnSquareIcon className="w-4 h-4 ml-2" />
       </div>
     </a>
   );
