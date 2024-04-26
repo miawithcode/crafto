5c8ff1cd8b6031cc24d3ece43a9a5e848960e0e7
miawithcode <cuttontail@gmail.com>
2024-04-26 11:23:19 +0300

    style: add resource card hover overlay

--- src/components/sections/Resources/ResourceCard.jsx
+++ src/components/sections/Resources/ResourceCard.jsx
@@ -1,3 +1,4 @@
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
 const ResourceCard = ({ resource }) => {
@@ -7,7 +8,7 @@ const ResourceCard = ({ resource }) => {
     <a
       target="_blank"
       href={url}
-      className="group relative rounded-lg bg-bgGray px-5 py-4 shadow-sm transition hover:shadow-md"
+      className="group group relative rounded-lg bg-bgGray px-5 py-4 shadow-sm transition hover:shadow-md"
     >
       <div className="relative h-[25vh] w-full lg:h-[20vh]">
         <Image
@@ -23,6 +24,11 @@ const ResourceCard = ({ resource }) => {
         <p className="pt-2 text-sm tracking-tight text-textMuted">
           {description}
         </p>
+      </div>
+
+      {/* Hover Overlay */}
+      <div className="absolute bottom-0 left-0 hidden h-1/6 w-full rounded-b-lg bg-bgBlack text-white group-hover:flex group-hover:justify-center group-hover:items-center">
+        Visit site <ArrowTopRightOnSquareIcon className="w-4 h-4 ml-2" />
       </div>
     </a>
   );
